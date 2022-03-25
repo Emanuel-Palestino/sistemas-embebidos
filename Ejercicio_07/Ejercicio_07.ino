31c2b7df12a87bdc7ab75f961aad88429f8805db
Emanuel <emanuel.palestino.h@gmail.com>
2022-03-25 09:32:17 -0600

    [Ejercicio_07] Versión utilizando ensamblador

--- Ejercicio_07/Ejercicio_07.ino
+++ Ejercicio_07/Ejercicio_07.ino
@@ -1,18 +1,15 @@
-// Realice un decodificador de binario a 7 segmentos, utilizando un arreglo de constantes en memoria Flash
+// Realice un decodificador de binario a 7 segmentos, utilizando un arreglo de constantes en memoria Flash USANDO ENSAMBLADOR
 
-const uint8_t tabla[] PROGMEM = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x67, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71};
+extern "C" void conf_IO();
+extern "C" void decodifica();
 
 void setup()
 {
-	DDRC = 0x00;
-	PORTC = 0xFF;
-	DDRD = 0xFF;
+	conf_IO();
 }
 
 void loop()
 {
-	uint8_t x;
-	x = PINC & 0x0F;
-	PORTD = pgm_read_byte(&tabla[x]);
+	decodifica();
 	delay(300);
 }
