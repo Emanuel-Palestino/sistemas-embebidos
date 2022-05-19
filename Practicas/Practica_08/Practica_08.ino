422c127df343a5f70f0a8961cc2a49c54574d792
Axel García <axldorian3@gmail.com>
2022-05-18 19:01:47 -0500

    Practica 08 terminada

--- Practicas/Practica_08/Practica_08.ino
+++ Practicas/Practica_08/Practica_08.ino
@@ -3,40 +3,35 @@
 uint8_t actual;
 uint8_t x = 0, y = 0;
 
-void setup()
-{
-  DDRC = 0x00;
+void setup(){
+  DDRC = 0x00;      // Entradas A4, A5
 
   // Configuración de Timer 1
   TCCR1A = 0x00;    // Modo CTC
-  TCCR1B = 0x0B;    // Inicia sin reloj
+  TCCR1B = 0x0B;    // Inicia con preescalor de 64
   TCCR1C = 0x00;
   OCR1A = 37499;    // Valor Máximo
-  TIMSK1 = 0x02;
+  TIMSK1 = 0x02;    // Habilita interrupción
 
-  ADMUX = 0x60;     // Canal 0, Vref en Avcc y alineación a la izquierda
-  ADCSRA = 0x8F;    // Hab. ADC, inicia apagado, con Interrupción, sin carrera libre
-  DIDR0 = 0x10;     // Sin buffer digital en ADC4
-  actual = 0x10;
-
-  Serial.begin(9600);
+  ADMUX = 0x64;     // Canal 4, Vref en Avcc y alineación a la izquierda
+  ADCSRA = 0xCF;    // Hab. ADC, inicia prendido, con interrupción, sin carrera libre
+  DIDR0 = 0x30;     // Sin buffer digital en ADC4, ADC5
+  actual = 0x64;    // Entrada actual
 
   initDisplays();
 }
 
 ISR(TIMER1_COMPA_vect){
-  actual ^= 0x30;
-  DIDR0 = actual;
-  ADCSRA |= 1 << ADSC;  // inicia conversión
+  actual ^= 0x01;       // Conmuta entre A4 y A5
+  ADMUX = actual;       // Actualiza ADMUX
+  ADCSRA |= 1 << ADSC;  // Inicia conversión
 }
 
 ISR(ADC_vect){
-  uint8_t aux = ADCH;
-  aux = (aux * 255) / 99;
+  uint8_t aux = ADCH;     // Obtiene valor del ADC
+  aux = (aux * 99) / 255; // Mapea coordenadas entre 0 y 99
 
-  Serial.println(ADCH);
-
-  if (actual == 0x10){
+  if (actual == 0x64){    // Sí es la coordenada X
     x = aux;
   } else {
     y = aux;
